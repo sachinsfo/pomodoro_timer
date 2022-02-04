e70fa2e8e9db4cb1997a649d03a11dc6038dcd9f
sachinsfo <sachinchepuri2@gmail.com>
2022-02-04 15:49:14 -0800

    Updated config to dynamically assign a port

--- pomodoro-timer/server.js
+++ pomodoro-timer/server.js
@@ -10,4 +10,12 @@ app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname + "/dist/pomodoro-timer/index.html"));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000, function () {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
+});
+
+//app.listen(process.env.PORT || 8080);
