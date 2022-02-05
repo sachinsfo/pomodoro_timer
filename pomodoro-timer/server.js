bb124ada6645fa3135fa20f0c5abb713308995fa
sachinsfo <sachinchepuri2@gmail.com>
2022-02-04 16:33:07 -0800

    Added more changes to the configuration

--- pomodoro-timer/server.js
+++ pomodoro-timer/server.js
@@ -4,11 +4,19 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static(__dirname + "/dist/pomodoro-timer"));
+//app.use(express.static(__dirname + "./dist/pomodoro-timer"), function () {
+//console.log(`directory name: ${__dirname}/dist/pomodoro-timer`);
+//});
 
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/pomodoro-timer/index.html"));
-});
+//app.get("/*", function (req, res) {
+//res.sendFile(path.join(__dirname + "/dist/pomodoro-timer/index.html"));
+//console.log(`directory name: ${__dirname}/dist/pomodoro-timer/index.html`);
+//});
+
+app.use(express.static("./dist/pomodoro-timer"));
+app.get("/*", (req, res) =>
+  res.sendFile("index.html", { root: "dist/pomodoro-timer/" })
+);
 
 app.listen(process.env.PORT || 3000, function () {
   console.log(
