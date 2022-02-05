266d117b7bd7059e4a2d73abdfe9b8eba363eaad
sachinsfo <sachinchepuri2@gmail.com>
2022-02-04 22:21:42 -0800

    trying a relative path - 2

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static(__dirname + "../pomodoro_timer"));
+app.use(express.static("/pomodoro_timer"));
 
 app.get("/*", function (req, res) {
-  const fullPath = path.join(__dirname + "../pomodoro_timer/index.html");
+  const fullPath = path.join("/pomodoro_timer/index.html");
   console.log(" Fetching from.." + fullPath);
   res.sendFile(fullPath);
 });
