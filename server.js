08dcb60035354e945d4cc6b0c0d187599cad0218
sachinsfo <sachinchepuri2@gmail.com>
2022-02-04 22:26:23 -0800

    trying a relative path - 3

--- server.js
+++ server.js
@@ -4,21 +4,21 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static("/pomodoro_timer"));
+// app.use(express.static("/pomodoro_timer"));
 
-app.get("/*", function (req, res) {
-  const fullPath = path.join("/pomodoro_timer/index.html");
-  console.log(" Fetching from.." + fullPath);
-  res.sendFile(fullPath);
-});
+// app.get("/*", function (req, res) {
+//   const fullPath = path.join("/pomodoro_timer/index.html");
+//   console.log(" Fetching from.." + fullPath);
+//   res.sendFile(fullPath);
+// });
 
-// app.use(express.static("./dist/pomodoro_timer"));
+app.use(express.static("./dist/pomodoro_timer"));
 
-// app.get("/*", (req, res) =>
-//   res.sendFile("index.html", { root: "dist/pomodoro_timer" })
-// );
+app.get("/*", (req, res) =>
+  res.sendFile("index.html", { root: "dist/pomodoro_timer" })
+);
 
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT || 8080, function () {
   console.log(
     "Express server listening on port %d in %s mode",
     this.address().port,
